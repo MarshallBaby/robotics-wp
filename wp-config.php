120ed161da1cf9145ab378fe1098d24500cde02c
MarshallBaby <aleksey.yatsevich@gmail.com>
2021-02-02 11:33:40 +0300

    0.0.1
    Co-Authored-By: VladsonX <75186030+VladsonX@users.noreply.github.com>

---
+++ wp-config.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'robotics' );
+
+/** MySQL database username */
+define( 'DB_USER', 'root' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', '' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'F86tk&bB=mHk2c]o~n@a-pHDCcUYzA=j./<MD6Tpmr!^0}HR?P:4mEQaM-4A;[&.' );
+define( 'SECURE_AUTH_KEY',  'rLdhKxRX.K3>pV+VxrZhW.!ZYS>bAV5OEW,A;ZWHdOrr>jd3]KTYc&fDb@5q{daR' );
+define( 'LOGGED_IN_KEY',    'la1`uN?=$5Hg?YOPhSHQH;;rEd(-o`hAr<j0|<sAV`I=|Gfm3&Bf5.<:x@Yj?F}z' );
+define( 'NONCE_KEY',        '^&3f&>6(Ro2`%R@/6e0ZtZo}9R1DMIJ&eA81?9q$h(!OSl>sj@n/E,EKy^sPGQ2d' );
+define( 'AUTH_SALT',        'X~w-UYD]Vk`B6G&CVw}Q<ORi^~3)VD{?|KJ{]-^+anU%kNSi*s5.Hn)gCYd[.^~o' );
+define( 'SECURE_AUTH_SALT', ' Y-3c/#_LzmFXV<^(N^d2(~3.0%f0Uso2Qw$5L.qX7WIQ@,xWpw$IckqE-wAhDKe' );
+define( 'LOGGED_IN_SALT',   'x.F=i(&Vfo{GrLdS9,{:D|ppU:>5rz)~o&jnD($[#Tp5F#(WDM4>0dFW<Tm@@U@X' );
+define( 'NONCE_SALT',       '$P)T:iZXqXi6i)v2YxJLsyJRlL$FJ:{rL&d>z_*9ECtYRShxdHSISal<PUuRH5s)' );
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
+
---
+++ wp-content/themes/robotics/index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Robotics</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link href="sass/css/style.css" rel="stylesheet">
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.4.min.js"></script>
+</head>
+
+<body>
+    <?php @include "header.php" ?>
+    <div class="container">
+        <section class="card">
+            <div class="card__background"></div>
+            <h1 class='card__title'>
+                Лаборатория робототехники БрГТУ
+            </h1>
+            <div class='card__descr'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
+                pretium quis, sem. Nulla consequat massa quis enim.
+            </div>
+            <div class='card__wrapper'>
+                <div class='card__button'>
+                    Наша деятельность
+                </div>
+                <a class='card__history card__history_hidden'>
+                    История <i class="bi bi-chevron-right"></i>
+                </a>
+            </div>
+
+            <div class="card__media">
+                <i class="bi bi-facebook"></i>
+                <i class="bi bi-instagram"></i>
+                <i class="bi bi-telegram"></i>
+                <i class="bi bi-youtube"></i>
+            </div>
+            <div class='aboba'>
+                <a class='card__history'>
+                    История <i class="bi bi-chevron-right"></i>
+                </a>
+            </div>
+        </section>
+        <section class='news'>
+            <div class='container'>
+                <h2 class='news__title'>Новости</h2>
+            </div>
+            <div class='container d-flex flex-column align-items-center'>
+                <div class="row equal">
+                    <div class="news__card col-xs-12 col-sm-6 col-lg-4">
+                        <div class='news__sticker'>
+                            <div class="news__thumb" style="background-image: url(img/news/1.jpg);"></div>
+                            <div class='news__help-container'>
+                                <h3 class='news__subtitle'>Второе место на WorldSkills СНГ</h3>
+                                <div class='news__descr'>19-23 мая 2015 года в Казани (Россия) прошел чемпионат по профессиональному мастерству «WorldSkills СНГ». Наши ребята заняли второе место!</div>
+                                <div class='news__more'>
+                                    <a href="single.php">Подробнее</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="news__card col-xs-12 col-sm-6 col-lg-4">
+                        <div class='news__sticker'>
+                            <div class="news__thumb" style="background-image: url(img/news/2.jpg);"></div>
+                            <div class='news__help-container'>
+                                <h3 class='news__subtitle'>Чемпионы WorldSkills 2014</h3>
+                                <div class='news__descr'>19-23 мая 2015 года в Казани (Россия) прошел чемпионат по профессиональному мастерству «WorldSkills СНГ». Наши ребята заняли второе место!</div>
+                                <div class='news__more'>
+                                    <a href="#">Подробнее</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="news__card col-xs-12 col-sm-6 col-lg-4">
+                        <div class='news__sticker'>
+                            <div class="news__thumb" style="background-image: url(img/news/3.jpg);"></div>
+                            <div class='news__help-container'>
+                                <h3 class='news__subtitle'>Arduino Day 2014</h3>
+                                <div class='news__descr'>19-23 мая 2015 года в Казани (Россия) прошел чемпионат по профессиональному мастерству «WorldSkills СНГ». Наши ребята заняли второе место!</div>
+                                <div class='news__more'>
+                                    <a href="#">Подробнее</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="news__card d-none d-sm-flex d-lg-none col-xs-12 col-sm-6 col-lg-4">
+                        <div class='news__sticker'>
+                            <div class="news__thumb" style="background-image: url(img/news/1.jpg);"></div>
+                            <div class='news__help-container'>
+                                <h3 class='news__subtitle'>Arduino Day 2014</h3>
+                                <div class='news__descr'>19-23 мая 2015 года в Казани (Россия) прошел чемпионат по профессиональному мастерству «WorldSkills СНГ». Наши ребята заняли второе место!</div>
+                                <div class='news__more'>
+                                    <a href="#">Подробнее</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class='news__all-news'>
+                    <a href="news.php">все новости</a>
+                </div>
+            </div>
+        </section>
+
+        <section class="projects">
+            <div class="container d-flex flex-column">
+                <h2 class ='projects__title'>Проекты</h2>
+                <div class='projects__descr'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                
+                </div>
+                
+                <div class="projects__wrapper align-self-center">
+                    <div class='projects__item' style="background-image:url(img/projects/IMG_3959.jpg)">
+                        <div class="projects__dark"></div>
+                    <div class='projects__name'>Проектирование робототехнических комплексов</div>
+                    </div>
+                    <div class='projects__item' style="background-image:url(img/projects/IMG_3967.jpg)">
+                        <div class="projects__dark"></div>
+                        <div class='projects__name'>Цифровое моделирование</div>
+                    
+                    </div>
+                    <div class='projects__item' style="background-image:url(img/projects/IMG_3975.jpg)">
+                        <div class="projects__dark"></div>
+                        <div class='projects__name'>Промышленная маркировка товаров</div>
+                    
+                    </div>
+                    <div class='projects__item' style="background-image:url(img/projects/IMG_3977.jpg)">
+                        <div class="projects__dark"></div>
+                        <div class='projects__name'>Разработка систем технического зрения</div>
+                    
+                    </div>
+                    <div class='projects__item' style="background-image:url(img/projects/IMG_3979.jpg)">
+                        <div class="projects__dark"></div>
+                        <div class='projects__name'>Промышленная маркировка товаров</div>
+                    
+                    </div>
+                    <div class='projects__item d-none d-lg-flex' style="background-image:url(img/projects/IMG_3988-Pano.jpg)">
+                        <div class="projects__dark"></div>
+                        <div class='projects__name'>Аддитивные технологии</div>
+                    
+                    </div>
+                    <div class='projects__item d-none d-lg-flex' style="background-image:url(img/projects/IMG_4015.jpg)">
+                        <div class="projects__dark"></div>
+                        <div class='projects__name'>Обучение и переподготовка</div>
+                    
+                    </div>
+                    <div onclick="location.href = 'projects.php';" class='projects__item' style="background-image:url(img/projects/IMG_4032.jpg)">
+                        <div class="projects__dark"></div>
+                        <div class='projects__name'>Все проекты</div>
+                    
+                    </div>
+                </div>
+               
+                
+            </div>
+        </section>
+
+        <section class='partners'>
+            <div class='container'>
+                <h2 class ='partners__title'>Наши партнеры</h2>
+                <div class='partners__descr'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                
+                </div>
+                <a class='partners__link' href='#'>Как стать партнером</a>
+                <div class='partners__wrapper'>
+                <div class='row '>
+                    <div class='justify-content-center d-flex flex-wrap col-sm-12 col-md-6 col-lg-4'>
+                        <div class='partners__item'>
+                           <div class='partners__logo'>
+                                <img src="img/patners/logo-savushkin-produkt.png" alt="partners">
+                           </div>
+                           <div class='partners__company'>
+                           ООО Системы промышленной автоматизации
+                           </div>
+                        </div>
+                    </div>
+                    <div class='justify-content-center d-flex flex-wrap col-sm-12 col-md-6 col-lg-4'>
+                        <div class='partners__item'>
+                           <div class='partners__logo'>
+                                <img src="img/patners/logo-savushkin-produkt.png" alt="partners">
+                           </div>
+                           <div class='partners__company'>
+                           ООО Системы промышленной автоматизации
+                           </div>
+                        </div>
+                    </div>
+                    <div class='justify-content-center d-flex flex-wrap col-sm-12 col-md-6 col-lg-4'>
+                        <div class='partners__item'>
+                           <div class='partners__logo'>
+                                <img src="img/patners/logo-savushkin-produkt.png" alt="partners">
+                           </div>
+                           <div class='partners__company'>
+                           ООО Системы промышленной автоматизации
+                           </div>
+                        </div>
+                    </div>
+                </div>
+                <div class='partners__full-list'>
+                    <a href="partners.php">полный список</a>
+                </div>
+                <hr class='partners__line'>
+                </div>
+            </div>
+        </section>
+        <section class='excursion'>
+            <div class='container'>
+                <h2 class='excursion__title'>Экскурсии</h2>
+            </div>
+            <div class='row justify-content-around'>
+                <div class=' flex-wrap d-flex justify-content-center col-sm-12 col-md-4 col-lg-4'>
+                    
+                    <div class='excursion__item' style="background-image: url(img/excursion/1.jpg);"> <span>Онлайн-экскурсия </span>
+                    <div class="excursion__dark"></div>
+                    </div>
+                </div>
+                <div class=' flex-wrap d-flex justify-content-center col-sm-12 col-md-4 col-lg-4'>
+                    
+                    <div class='excursion__item' style="background-image: url(img/excursion/2.jpg);"> <span>Оффлайн-экскурсия </span>
+                    <div class="excursion__dark"></div>
+                    </div>
+                </div>
+                <div class=' flex-wrap d-flex justify-content-center col-sm-12 col-md-4 col-lg-4'>
+                    
+                    <div class='excursion__item' style="background-image: url(img/excursion/3.jpg);"> <span>Виртуальная-экскурсия </span>
+                    <div class="excursion__dark"></div>
+                    </div>
+                </div>
+            </div>
+            <hr class='excursion__line'>
+            
+        </section>
+        <section class='links mb-5'>
+            <div class='container'>
+                <h2 class="links__title">Полезные ссылки</h2>
+                <div class="container">
+                    <div class='row'>
+                        <div class='col-sm-12 col-lg-4 col-xl-4 col-xxl-3'>
+                            <div class='links__wrapper'>
+                                <div data-href="sfs" data-title="RoboRace" class='links__item links__item_active'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum animi commodi dolore sit iusto est reiciendis minus in ut, necessitatibus doloremque magnam aliquam quae! Corporis illum voluptas incidunt facilis porro.
+                                </div>
+                                <div data-href="sefs" data-title="TowerLight Urcap" class='links__item'>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur quibusdam ea doloribus aperiam maxime vel officiis quidem, doloremque recusandae maiores. Cupiditate beatae doloremque voluptates sequi magnam? Facere reiciendis molestiae voluptate?
+                                </div>
+                                <div data-href="dgdg" data-title="WIKI" class='links__item'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In a vitae nemo nostrum beatae! Vero iste, iure et exercitationem asperiores ullam saepe cupiditate, ducimus repellat magni repudiandae. Quaerat, facilis suscipit.
+                                </div>
+                                <div data-href="hds" data-title="RAIPAP" class='links__item'>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, ipsam est nam, modi sunt id minus assumenda nobis aliquam iste cum nisi officia dolor minima quasi? Quos nostrum expedita a.
+                                </div>
+                                <div data-href="adafg" data-title="Публикации" class='links__item'>
+                                    Перечень научных публикаций лаборатории, тем дипломных проектов, магистерских диссертаций, участие в конференциях и семинарах.
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class='offset-lg-1 col-lg-7 offset-xl-0 col-xl-6 col-xxl-6 d-none d-lg-flex'>
+                            <div class="links__info">
+                                <div class='links__subtitle'>RoboRace</div>
+                                <div class='links__desrc'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</div>
+                                <a href="#" class="links__href"> Подробнее </a>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </div>
+    <?php include "footer.php" ?>
+    <script src="script/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+
+</html>
