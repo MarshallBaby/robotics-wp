f67bedd7deaaf184f3b70db3ff90f6359d8a4a3e
MarshallBaby <aleksey.yatsevich@gmail.com>
2021-02-04 15:24:19 +0300

    0.0.9 Projects
    Co-Authored-By: VladsonX <75186030+VladsonX@users.noreply.github.com>

--- wp-content/themes/robotics/projects.php
+++ wp-content/themes/robotics/projects.php
@@ -4,7 +4,35 @@ Template Name: projects
 */
 get_header();
 ?>
+<section class='projects'>
+    <div class='container'>
+        <h2 class='projects__title'>Проекты</h2>
+        <div class='row'>
+            <?php
+            $args = array(
+                'numberposts' => -1,
+                'category_name' => 'projects',
 
-<?php 
+            );
+            $posts = get_posts($args);
+            foreach ($posts as $post) {
+            ?>
+                <div onclick="location.href = '<?php the_permalink(); ?>';" class='col-sm-12 col-md-6 col-lg-4'>
+                    <div class='projects__item projects__item_ext' style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
+                        <span> <?php the_title(); ?> </span>
+                        <div class="projects__dark"></div>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
+
+
+        </div>
+    <a class='article__back align-self-start' href="<?php echo home_url(); ?>">Назад</a>
+
+    </div>
+</section>
+<?php
 get_footer();
 ?>
