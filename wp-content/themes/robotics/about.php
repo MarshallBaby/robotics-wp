f564fe738b96fc7250de6beec18658e1bd0a5c87
MarshallBaby <aleksey.yatsevich@gmail.com>
2021-02-12 12:12:22 +0300

    0.0.18
    Co-Authored-By: Vladislav Novitskiy <75186030+VladsonX@users.noreply.github.com>

--- wp-content/themes/robotics/about.php
+++ wp-content/themes/robotics/about.php
@@ -26,6 +26,31 @@ get_header();
             }
 
             ?>
+             <?php
+                $args = array(
+                    'post_type' => 'page', //it is a Page right?
+                    'post_status' => 'publish',
+                    'meta_query' => array(
+                        array(
+                            'key' => '_wp_page_template',
+                            'value' => ['members.php'], 
+                        )
+                    )
+                );
+                $query = new WP_Query($args);
+                while ($query->have_posts()) {
+                    $query->the_post();
+                ?>
+                    <div class=' flex-wrap d-flex justify-content-center col-sm-12 col-md-4 col-lg-3'>
+                    <div onclick="location.href = '<?php the_permalink(); ?>';" class='excursion__item' style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
+                        <span><?php the_title(); ?></span>
+                        <div class="excursion__dark"></div>
+                    </div>
+                </div>
+                <?php
+
+                }
+                ?>
             <a class='article__back align-self-start' href='<?php echo home_url(); ?>'>Назад</a>
         </div>
     </div>
---
+++ wp-content/themes/robotics/members.php
@@ -0,0 +1,68 @@
+<?php
+/*
+Template Name: members
+*/
+get_header();
+?>
+
+<section class='members '>
+    <div class='container'>
+        <h2 class='members__title'> <?php the_title(); ?></h2>
+        <div class='members__wrapper'>
+            <div class='row justify-content-start'>
+                <div class='col-sm-12 col-md-6 col-lg-9'>
+                    <?php
+                    $posts = get_posts(array(
+                        'category_name' => 'members',
+                        'numberposts' => -1,
+                        'orderby' => 'menu_order'
+                    ));
+                    foreach ($posts as $post) {
+                    ?>
+                        <div class='members__item d-flex flex-column flex-lg-row'>
+                            <div class='members__thumb' style="background-image: url(<?php the_post_thumbnail_url(); ?>);"></div>
+                            <div class='members__info'>
+                                <h3 class='members__name'><?php the_title(); ?></h3>
+                                <div class='members__descr'> <?php the_excerpt(); ?>
+
+                                    <span class='d-none d-md-flex members__descr_sub'> <?php the_content(); ?> </span>
+                                </div>
+                            </div>
+
+                        </div>
+                    <?php
+                    }
+                    ?>
+            </div>
+            <hr class='members__line'>
+        </div>
+
+</section>
+<section class="mb-5">
+    <div class='container'>
+        <?php
+                $args = array(
+                    'post_type' => 'page', //it is a Page right?
+                    'post_status' => 'publish',
+                    'meta_query' => array(
+                        array(
+                            'key' => '_wp_page_template',
+                            'value' => ['members.php'], // template name as stored in the dB
+                    
+                        )
+                    )
+                );
+                $query = new WP_Query($args);
+                while ($query->have_posts()) {
+       
+                    ?>  <div class='students__title'>Студенты</div> <?php
+                    $query->the_post();
+                    the_content();
+                }
+                ?>
+        <a class='article__back align-self-start ml-3' href="<?php echo home_url(); ?>">Назад</a>
+    </div>
+
+</section>
+
+<?php get_footer(); ?>
